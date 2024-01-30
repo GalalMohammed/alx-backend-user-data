3fc8537e5d9e944bf28a87e593424a83074996c1
GalalMohammed <galalmoh2001@gmail.com>
2024-01-30 17:53:10 +0200

    use typing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,9 +5,10 @@ This module defines filter_datum function.
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Get the log message obfuscated.
     """
