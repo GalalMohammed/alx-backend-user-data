db30f647d6650dd13fbf5c15014010fb12b146ac
GalalMohammed <galalmoh2001@gmail.com>
2024-01-31 21:00:15 +0200

    add task 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,19 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+import werkzeug
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+@app.errorhandler(401)
+def unauthorized(error: werkzeug.exceptions.Unauthorized) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(404)
