fc7371ea31ff54274bee4d8459cff91d3c4cc2d0
GalalMohammed <galalmoh2001@gmail.com>
2024-01-31 21:03:17 +0200

    add task 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,6 +22,13 @@ def unauthorized(error: werkzeug.exceptions.Unauthorized) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden(error: werkzeug.exceptions.Forbidden) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
