038df63ecffaa79ce669c51d925f0c1f7fbd7f47
GalalMohammed <galalmoh2001@gmail.com>
2024-01-31 21:55:45 +0200

    add task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module for Basic auth.
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic auth API authentication manager.
+    """
