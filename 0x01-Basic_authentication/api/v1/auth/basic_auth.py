8e41b7d393961e159e3b211d7cd4cced52334779
GalalMohammed <galalmoh2001@gmail.com>
2024-01-31 22:22:46 +0200

    add task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 Module for Basic auth.
 """
+import base64
+import binascii
 from .auth import Auth
 
 
@@ -17,3 +19,14 @@ class BasicAuth(Auth):
         if authorization_header and isinstance(authorization_header, str) and\
                 authorization_header.startswith("Basic "):
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decode value of a Base64 string.
+        """
+        if base64_authorization_header and isinstance(
+                base64_authorization_header, str):
+            try:
+                return base64.b64decode(base64_authorization_header).decode('utf-8')
+            except binascii.Error:
+                return None
