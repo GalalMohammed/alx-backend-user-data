c769b541cc282c74f4d3ef41e48615d086e290f0
GalalMohammed <galalmoh2001@gmail.com>
2024-02-05 01:16:16 +0200

    add task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module for Session auth.
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session auth API authentication manager.
+    """
