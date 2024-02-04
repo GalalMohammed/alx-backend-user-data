ce3f23a133a1907bc134eef4a6a31edd9e6989c3
GalalMohammed <galalmoh2001@gmail.com>
2024-02-05 01:28:13 +0200

    add task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,23 @@
 """
 Module for Session auth.
 """
+import uuid
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Session auth API authentication manager.
     """
+
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id.
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
