a33fdb82b6085520737ac981c6f35bf9c0e37fad
GalalMohammed <galalmoh2001@gmail.com>
2024-02-05 22:49:37 +0200

    add task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,15 @@ class Auth:
             raise ValueError(f"User {user.email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode(), user.hashed_password):
+                return True
+            else:
+                return False
+        else:
+            return False
