53744259a0ea484ad392f8d3fd6995b7bfde2ae6
GalalMohammed <galalmoh2001@gmail.com>
2024-02-06 00:32:05 +0200

    fix style

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,7 +91,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            self._db.update_user(user.id, hashed_password=_hash_password(password),
-                            reset_token=None)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
